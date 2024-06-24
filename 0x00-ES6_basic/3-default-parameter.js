edfd8ff9cd5b8618dce01a278ea0d82697befd33
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-06-25 00:43:52 +0100

    refactor: Update default parameters in getSumOfHoods function

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,4 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  return (initialNumber + (expansion1989 == undefined ? 89 : expansion1989) + (expansion2019 == undefined ? 19 : expansion2019));
+/* eslint-disable */
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019
 }
