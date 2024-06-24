c1e3085d91b545d88c4b10fec742ddbb6be4aa7a
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-06-25 00:38:21 +0100

    feat: Add default parameter to getSumOfHoods function

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,3 @@
+export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+  return (initialNumber + (expansion1989 == undefined ? 89 : expansion1989) + (expansion2019 == undefined ? 19 : expansion2019));
+}
