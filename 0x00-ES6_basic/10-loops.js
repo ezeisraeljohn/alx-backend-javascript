8b48794f9542bb50b07c7bd0d73cd2056994c224
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-06-27 04:12:03 +0100

    feat: Add appendToEachArrayValue function in ES6 module

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+        for (let [index, value] of array.entries()) {
+          array[index] = appendString + value;
+        }
+      
+        return array;
+      }
+      
