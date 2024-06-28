6cdb2523ae5e8337b8abee2107f4710233e5f1c5
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-06-28 11:09:28 +0100

    changes made

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,12 @@
+/*eslint disable*/
+import ClassRoom from "./0-classroom.js";
+
+const initializeRooms = () => {
+        const room1 = new ClassRoom(19);
+        const room2 = new ClassRoom(20);
+        const room3 = new ClassRoom(34);
+        
+        return [room1, room2, room3];
+        };
+
+export default initializeRooms;
