9a40c27bbc7bfcfa6fd9c3c92cc3eb938ecf35fe
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-06-28 12:05:33 +0100

    Added some changes to this file

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,6 +2,15 @@
 
 class HolbertonCourse {
   constructor(name, length, students) {
+        if (typeof name !== 'string') {
+                throw new TypeError('Name must be a string');
+              }
+              if (typeof length !== 'number') {
+                throw new TypeError('Length must be a number');
+              }
+              if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+                throw new TypeError('Students must be an array of strings');
+              }
     this._name = name;
     this._length = length;
     this._students = students;
@@ -20,15 +29,25 @@ class HolbertonCourse {
   }
 
   set name(newName) {
+    if (typeof newName != 'string'){
+        throw new TypeError("Name must be a string")
+    }
     this._name = newName;
   }
 
   set length(newLength) {
+    if (typeof newLength != 'number'){
+        throw new TypeError("Length must be a number")
+    }
     this._length = newLength;
   }
 
   set students(newStudents) {
+    if (!Array.isArray(newStudents)){
+        throw new TypeError("Students must be an array")
+    }
     this._students = newStudents;
   }
 }
+
 export default HolbertonCourse;
