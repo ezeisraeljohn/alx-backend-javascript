8df01a16f60ddfa68d1b2eb506fc1eaa8409a69c
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-02 16:55:31 +0100

    feat: Update student data in getListStudents function

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -6,7 +6,7 @@ function getListStudents(){
         const data = [
                 {id: 1, firstname: "Guillaume", location: "San Francisco"},
                 {id: 2, firstname: "James", location: "Columbia"},
-                {id: 3, firstname: "Serena", location: "San Francisco"}
+                {id: 5, firstname: "Serena", location: "San Francisco"}
         ]
         array.push(...data)
         return array
