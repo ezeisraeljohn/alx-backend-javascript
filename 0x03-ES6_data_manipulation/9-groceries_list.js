dd7578f3b30cd2ce130d576e7659b17ea1a3cdd6
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-03 00:47:39 +0100

    Added some changes

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,15 @@
+/*eslint-disable*/
+
+
+const groceriesList = ()=>{
+        const myMap = new Map()
+        myMap.set("Apples", 10)
+        myMap.set("Tomatoes", 10)
+        myMap.set("Pasta", 1)
+        myMap.set("Rice", 1)
+        myMap.set("Banana", 1)
+
+        return myMap
+}
+
+export default groceriesList
