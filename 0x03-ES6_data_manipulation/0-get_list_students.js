9d889bdf92f57a6043a4d07a7239a3ec7e23f8d9
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-02 17:32:22 +0100

    refactor: Update property name in getListStudents function

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,9 +4,9 @@
 function getListStudents(){
         const array = new Array()
         const data = [
-                {id: 1, firstname: "Guillaume", location: "San Francisco"},
-                {id: 2, firstname: "James", location: "Columbia"},
-                {id: 5, firstname: "Serena", location: "San Francisco"}
+                {id: 1, firstName: "Guillaume", location: "San Francisco"},
+                {id: 2, firstName: "James", location: "Columbia"},
+                {id: 5, firstName: "Serena", location: "San Francisco"}
         ]
         array.push(...data)
         return array
