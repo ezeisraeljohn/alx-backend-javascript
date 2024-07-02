ec685c00e92597c6d00d8f1725dbcb52e36c827f
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-07-02 19:33:13 +0100

    refactor: Update getListStudents function to use consistent formatting and naming conventions

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,10 +4,10 @@
 function getListStudents(){
         const array = new Array()
         const data = [
-                {id: 1, firstName: "Guillaume", location: "San Francisco"},
-                {id: 2, firstName: "James", location: "Columbia"},
-                {id: 5, firstName: "Serena", location: "San Francisco"}
-        ]
+                { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+                { id: 2, firstName: 'James', location: 'Columbia' },
+                { id: 5, firstName: 'Serena', location: 'San Francisco' }
+              ]
         array.push(...data)
         return array
 }
